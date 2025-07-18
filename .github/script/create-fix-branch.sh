da191216cbb0189653c3b3ab41ebee1204d9dfd4
Marcos Lorenzo <11718206+ml0renz0@users.noreply.github.com>
2025-07-18 12:59:35 +0200

    feature: added `finalize release` step

--- .github/script/create-fix-branch.sh
+++ .github/script/create-fix-branch.sh
@@ -6,19 +6,9 @@ echo "Set committer details"
 git config user.name github-actions[bot]
 git config user.email github-actions[bot]@users.noreply.github.com
 
-echo "Create hotfix branch"
-RELEASE_BRANCH=hotfix-v1.0.1
+echo "Create fix branch"
 git checkout main
-git pull origin main
-git checkout -b $RELEASE_BRANCH
-
-echo "Push release branch"
-git commit --allow-empty --message="Empty commit to initialize branch"
-git push --set-upstream origin $RELEASE_BRANCH
-
-echo "Create feature branch"
-git checkout main
-FEATURE_BRANCH=fix-game-background
+FEATURE_BRANCH=fix/game-background
 git checkout -b $FEATURE_BRANCH
 
 echo "Make changes to files"
